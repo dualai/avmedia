28156da77c8dbaa6d04b200085cd75877bb3e08a
Andy <Andy@gmail.com>
2019-02-13 23:33:57 +0800

    first

---
+++ app/src/main/java/son/dualai/avmedia/MainActivity.java
@@ -0,0 +1,25 @@
+package son.dualai.avmedia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+//        OutputStream
+//        ByteArrayOutputStream
+//        FileOutputStream
+//        OutputStreamWriter
+        
+    }
+}
